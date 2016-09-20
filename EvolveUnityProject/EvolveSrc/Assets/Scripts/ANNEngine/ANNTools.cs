818d4006841cc0f3f6c590093464a23d58f50b21
Roman <steelknight93@gmail.com>
2016-09-20 12:57:59 +0300

    add some test scripts

---
+++ EvolveUnityProject/EvolveSrc/Assets/Scripts/ANNEngine/ANNTools.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ANNTools
+{
+    public static float Sigmoid(float x)
+    {
+        return 1 / (1 + Mathf.Exp(-x));
+    }
+}
---
+++ EvolveUnityProject/EvolveSrc/Assets/Scripts/SignalTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignalTester : MonoBehaviour
+{
+    private void Update()
+    {
+        if ((int)Time.time % 3 == 0)
+        {
+            float randomSignalPower = Random.Range(-0.57f, 3.66f);
+            Debug.LogFormat("input signal {0}, sigmoid modification {1}", randomSignalPower, ANNTools.Sigmoid(randomSignalPower));
+        }
+    }
+}
